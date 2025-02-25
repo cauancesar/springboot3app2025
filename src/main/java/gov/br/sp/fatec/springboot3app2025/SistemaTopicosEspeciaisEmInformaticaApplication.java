5acaa03334f2082979094a05d216dafd848a982d
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2025-02-25 10:06:31 -0300

    first commit

---
+++ src/main/java/gov/br/sp/fatec/springboot3app2025/SistemaTopicosEspeciaisEmInformaticaApplication.java
@@ -0,0 +1,13 @@
+package gov.br.sp.fatec.springboot3app2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTopicosEspeciaisEmInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTopicosEspeciaisEmInformaticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/br/sp/fatec/springboot3app2025/SistemaTopicosEspeciaisEmInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.br.sp.fatec.springboot3app2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTopicosEspeciaisEmInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
